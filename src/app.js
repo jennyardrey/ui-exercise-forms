1899e0ff60352ce66b1ff51f5b9c27f2fc3ca2e6
Jenny Ardrey <jenny@ardrey.co.uk>
2020-11-23 14:57:17 +0000

    added reset instructions that show and hide on click

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const App = () => {
     email: "",
     success: false,
     error: false,
+    instructions: true,
   });
 
   const handleChange = (e) => {
@@ -34,6 +35,21 @@ const App = () => {
         }));
       });
   };
+
+  const revealDiv = (e) => {
+    e.preventDefault();
+    setValues((values) => ({
+      instructions: true,
+    }));
+  };
+
+  const hideDiv = (e) => {
+    e.preventDefault();
+    setValues((values) => ({
+      instructions: false,
+    }));
+  };
+
   return (
     <div className="reset-form">
       <h1>Forgot Your Password?</h1>
@@ -67,6 +83,40 @@ const App = () => {
           <button type="submit">Submit</button>
           <a href="">Go Back</a>
         </div>
+
+        {!values.instructions === true ? (
+          <a className="reveal-hide" onClick={revealDiv}>
+            Are you having trouble resetting your password? &darr;
+          </a>
+        ) : (
+          <a className="reveal-hide" onClick={hideDiv} type="button">
+            Hide instructions
+          </a>
+        )}
+        {!values.instructions === true ? null : (
+          <div className="reset-instructions">
+            <p>
+              We have had some customers who mistakenly believe they have a
+              store account when in fact they are only signed up to receive our
+              VIP Club newsletter
+            </p>
+            <p>
+              If you receive our newsletter but have never made a purchase
+              online this might apply to you, the best way to check this is to
+              do the following:
+            </p>
+            <ol>
+              <li>Go back to the register page</li>
+              <li> Register with your email address</li>
+              <p>If you do already have an account the form will tell you</p>
+            </ol>
+            <p>
+              If the form tells you you already have an account and the password
+              reset doesn't work please contact the Customer Service Team on 033
+              900 0094
+            </p>
+          </div>
+        )}
       </form>
     </div>
   );
