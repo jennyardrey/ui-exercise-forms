9573dc9ec828fa295f4bcfcf7fe009461e96de02
Jenny Ardrey <jenny@ardrey.co.uk>
2020-11-23 15:22:50 +0000

    added styling to match given designs

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const App = () => {
     email: "",
     success: false,
     error: false,
-    instructions: true,
+    instructions: false,
   });
 
   const handleChange = (e) => {
@@ -53,8 +53,8 @@ const App = () => {
   return (
     <div className="reset-form">
       <h1>Forgot Your Password?</h1>
-      <p>
-        Please enter you email address below to recieve a reset password link
+      <p id="sub-head">
+        Please enter you email address below to recieve a reset password link.
       </p>
 
       <form className="form" onSubmit={handleSubmit}>
@@ -68,55 +68,56 @@ const App = () => {
             Success, we have emailed your password reset link
           </div>
         )}
-
-        <label className="form-item">Email Address</label>
-        <input
-          className="form-item"
-          id="email-input"
-          name="email"
-          placeholder="example@email.com"
-          type="email"
-          value={values.email}
-          onChange={handleChange}
-        />
-        <div className="buttons">
-          <button type="submit">Submit</button>
-          <a href="">Go Back</a>
-        </div>
-
-        {!values.instructions === true ? (
-          <a className="reveal-hide" onClick={revealDiv}>
-            Are you having trouble resetting your password? &darr;
-          </a>
-        ) : (
-          <a className="reveal-hide" onClick={hideDiv} type="button">
-            Hide instructions
-          </a>
-        )}
-        {!values.instructions === true ? null : (
-          <div className="reset-instructions">
-            <p>
-              We have had some customers who mistakenly believe they have a
-              store account when in fact they are only signed up to receive our
-              VIP Club newsletter
-            </p>
-            <p>
-              If you receive our newsletter but have never made a purchase
-              online this might apply to you, the best way to check this is to
-              do the following:
-            </p>
-            <ol>
-              <li>Go back to the register page</li>
-              <li> Register with your email address</li>
-              <p>If you do already have an account the form will tell you</p>
-            </ol>
-            <p>
-              If the form tells you you already have an account and the password
-              reset doesn't work please contact the Customer Service Team on 033
-              900 0094
-            </p>
+        <div className="form-contents">
+          <label className="form-item">Email Address</label>
+          <input
+            className="form-item"
+            id="email-input"
+            name="email"
+            placeholder="example@email.com"
+            type="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+          <div className="buttons">
+            <button type="submit">Reset password</button>
+            <a href="">Go Back</a>
           </div>
-        )}
+
+          {!values.instructions === true ? (
+            <a className="reveal-hide" onClick={revealDiv}>
+              Are you having trouble resetting your password? &darr;
+            </a>
+          ) : (
+            <a className="reveal-hide" onClick={hideDiv}>
+              Hide instructions
+            </a>
+          )}
+          {!values.instructions === true ? null : (
+            <div className="reset-instructions">
+              <p>
+                We have had some customers who mistakenly believe they have a
+                store account when in fact they are only signed up to receive
+                our VIP Club newsletter
+              </p>
+              <p>
+                If you receive our newsletter but have never made a purchase
+                online this might apply to you, the best way to check this is to
+                do the following:
+              </p>
+              <ol>
+                <li>Go back to the register page</li>
+                <li> Register with your email address</li>
+                <p>If you do already have an account the form will tell you</p>
+              </ol>
+              <p>
+                If the form tells you you already have an account and the
+                password reset doesn't work please contact the Customer Service
+                Team on 033 900 0094
+              </p>
+            </div>
+          )}
+        </div>
       </form>
     </div>
   );
