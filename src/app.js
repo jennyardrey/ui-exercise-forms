75a043685795d9ee4fbf24cf6e50758f1391abca
Jenny Ardrey <jenny@ardrey.co.uk>
2020-11-23 14:29:45 +0000

    added main html form elements

--- src/app.js
+++ src/app.js
@@ -2,7 +2,31 @@ import React from "react";
 import "./app.css";
 
 const App = () => {
-  return <div>Here is something</div>;
+  return (
+    <div className="reset-form">
+      <h1>Forgot Your Password?</h1>
+      <p>
+        Please enter you email address below to recieve a reset password link
+      </p>
+
+      <form className="form" onSubmit="">
+        <label className="form-item">Email Address</label>
+        <input
+          className="form-item"
+          id="email-input"
+          name="email"
+          placeholder="example@email.com"
+          type="email"
+          value=""
+          onChange=""
+        />
+        <div className="buttons">
+          <button type="submit">Submit</button>
+          <a href="">Go Back</a>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default App;
